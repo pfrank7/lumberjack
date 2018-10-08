c43068e9f1f4b0f79d2e4019d392589356923c59
Paige Frank <paigefrank7@gmail.com>
2018-10-08 14:42:49 -0400

    Update scroll.js

--- assets/themes/custom-twitter/script/scroll.js
+++ assets/themes/custom-twitter/script/scroll.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
 	x = document.getElementById("fade");
-    if (x.scrollTop < 300) {
+    if (document.documentElement.scrollTop > 300) {
     	x.className = "fade-visible";
     }
     if (document.documentElement.scrollTop == 0) {
