2aef1251f700b3eec5d591a2f6bf00caf5cb3506
Paige Frank <paigefrank7@gmail.com>
2018-10-08 13:55:05 -0400

    Update scroll.js

--- assets/themes/custom-twitter/script/scroll.js
+++ assets/themes/custom-twitter/script/scroll.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
   var pageTop = $(document).scrollTop()
   var pageBottom = pageTop + $(window).height()
-  var tags = $(".fade")
+  var tags = $(document).getElementsByClassName("fade");
 
   for (var i = 0; i < tags.length; i++) {
     var tag = tags[i]
