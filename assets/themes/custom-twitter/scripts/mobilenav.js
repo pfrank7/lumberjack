010a26dc2ad6600da6f673f12ffff1dd0acbb7f5
Paige Frank <paigefrank7@gmail.com>
2018-11-30 21:56:19 -0500

    Update mobilenav.js

--- assets/themes/custom-twitter/scripts/mobilenav.js
+++ assets/themes/custom-twitter/scripts/mobilenav.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
+$(document).ready(function () {
+//toggle mobile menu links
+	$('#mobile-nav').click(function () {
+		$(this).toggleClass('open');
+		$(".mobile-links").toggleClass('open');
+		$(".mobile-links ul").toggleClass('open');
+		$(".main").toggleClass('blur');
+	});
+	$('.mobile-links a').click(function () {
+		if ($(this).attr('class') !== "expand_projects") {
+			$('#mobile-nav').toggleClass('open');
+			$(".mobile-links").toggleClass('open');
+			$(".mobile-links ul").toggleClass('open');
+			$(".main").toggleClass('blur');
+		}
+	});
+});
